bbbbe1422c3cffac75fe5c2aa2ba445491609da1
Gaurav Bhatnagar <bhatnagar.gaurav83@gmail.com>
2016-06-16 20:36:38 +0530

    Submitting the project for RecyclerView with Clickable functionality
    Incorporated the Clickable functionality for the RecyclerView with
    Simple Plain List.

---
+++ RecyclerViewClickList/app/src/main/java/com/upgrad/tutorial/recyclerviewclicklist/ItemAdapter.java
@@ -0,0 +1,32 @@
+package com.upgrad.tutorial.recyclerviewclicklist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by gaurav_bhatnagar on 6/15/2016.
+ */
+
+public class ItemAdapter extends RecyclerView.Adapter<RowHolderWithListener> {
+    MainActivity activity;
+
+    public ItemAdapter(MainActivity activity){
+        this.activity = activity;
+    }
+
+    @Override
+    public RowHolderWithListener onCreateViewHolder(ViewGroup parent, int viewType) {
+        return(new RowHolderWithListener(activity.getLayoutInflater()
+                .inflate(R.layout.row, parent, false)));
+    }
+
+    @Override
+    public void onBindViewHolder(RowHolderWithListener holder, int position) {
+        holder.bindModel(activity.shoppingItems[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return(activity.shoppingItems.length);
+    }
+}
---
+++ RecyclerViewClickList/app/src/main/java/com/upgrad/tutorial/recyclerviewclicklist/RowHolderWithListener.java
@@ -0,0 +1,49 @@
+package com.upgrad.tutorial.recyclerviewclicklist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by gaurav_bhatnagar on 6/15/2016.
+ */
+public class RowHolderWithListener extends RecyclerView.ViewHolder implements View.OnClickListener{
+    TextView itemLabel=null;
+    TextView magnitude=null;
+    ImageView avatar=null;
+    String template=null;
+
+    RowHolderWithListener(View row) {
+        super(row);
+
+        itemLabel=(TextView)row.findViewById(R.id.item_label);
+        magnitude=(TextView)row.findViewById(R.id.magnitude);
+        avatar=(ImageView)row.findViewById(R.id.avatar);
+
+        template=magnitude.getContext().getString(R.string.size_template);
+
+        // Setting the OnClickListener for the row
+        row.setOnClickListener(this);
+    }
+
+    void bindModel(String item) {
+        itemLabel.setText(item);
+        magnitude.setText(String.format(template, item.length()));
+
+        if (item.length()>4) {
+            avatar.setImageResource(R.drawable.recycle);
+        }
+        else {
+            avatar.setImageResource(R.drawable.great);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(),
+                String.format("Clicked on position %d", getAdapterPosition()+1),
+                Toast.LENGTH_SHORT).show();
+    }
+}
